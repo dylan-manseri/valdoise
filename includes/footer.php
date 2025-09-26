1de3d1d81132dd9a1a3640a403f838bf2bec6a5c
fari-za <131452056+fari-za@users.noreply.github.com>
2025-09-26 23:05:57 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,10 @@
-</main>
-<footer>
-    <!-- footer -->
-</footer>
+    </main>
+    <footer>
+        <span>CY Paris Cergy Université - 2025</span>
+        <span>
+            <a href="#" id="change-consent" style="color: #00D0FF;">Gérer les cookies</a>
+        </span>
+    </footer>
+
 </body>
 </html>
