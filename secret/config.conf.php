d98f0fe87b7fb0a0bb846dae84f01b15c38c8d31
Dylan Manseri <brywildpro@gmail.com>
2025-11-07 11:52:33 +0100

    Delete secret directory

--- secret/config.conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'hostname' => 'mysql-sortievaldoise.alwaysdata.net',
-    'username' => '437124',
-    'password' => "j'ai_mange3ratatouille&-1-pain",
-    'dbname' => 'sortievaldoise_database',
-    'port' => 3306
-];
